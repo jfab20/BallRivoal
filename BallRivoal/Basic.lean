b0be1976e45a7f2181038165cfe7f2bb8f335977
jfab20 <antonio.balderas.francisco@gmail.com>
2026-04-06 09:02:37 +0200

    Update README

--- BallRivoal/Basic.lean
+++ BallRivoal/Basic.lean
@@ -8,4 +8,7 @@ noncomputable def oddZeta : ℕ → ℝ := fun n ↦ (riemannZeta (2 * n + 3)).r
 
 #check Set.range oddZeta
 
+-- This is the goal. The dimension of the Q-vector space spanned by the set of
+-- all zeta(2 k +1)$ is of infinite dimension
+
 lemma not_fg_rat_span_range_oddZeta : ¬ (Submodule.span ℚ (Set.range oddZeta)).FG := sorry
